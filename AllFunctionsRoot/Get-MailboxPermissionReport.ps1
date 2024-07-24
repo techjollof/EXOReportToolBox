6b872ac035db4c3f73628fd229729ca5545437a2
TechJollof <techjollof@gmail.com>
2024-07-24 21:04:30 +0800

    Update Get-MailboxPermissionReport.ps1

--- AllFunctionsRoot/Get-MailboxPermissionReport.ps1
+++ AllFunctionsRoot/Get-MailboxPermissionReport.ps1
@@ -1,13 +1,12 @@
-#function Get-MailboxPermissionReport {
+function Get-MailboxPermissionReport {
     [CmdletBinding()]
     param (
-
-        [Parameter(ParameterSetName="SpecificMailboxes")]
+        [Parameter(ParameterSetName = "SpecificMailboxes")]
         [string[]]
         $MailboxAddress,
 
-        [Parameter(ParameterSetName="Bulk")]
-        [ValidateSet("UserMailbox","SharedMailbox","RoomMailbox","All")]
+        [Parameter(ParameterSetName = "Bulk")]
+        [ValidateSet("UserMailbox", "SharedMailbox", "RoomMailbox", "All")]
         $MailboxTypes = "All",
 
         [Parameter()]
@@ -15,39 +14,103 @@
         $ExpandedReport
     )
 
-    $permReport =@()
+    $permReport = @()
 
-    $permReport += foreach ($mbx in $MailboxAddress) {
-        $mailbox  = Get-Mailbox $mbx -ErrorAction SilentlyContinue
-        if ($null -ne $mailbox) {
-            $fullAccess = Get-MailboxPermission -Identity $mailbox.Identity | Where-Object { $_.User -ne "NT AUTHORITY\SELF" }
-            $sendAs = Get-RecipientPermission -Identity $mailbox.Identity | Where-Object { $_.Trustee -ne "NT AUTHORITY\SELF" }
-
-            #$sendOnBehalfCheck = 
-            $sendOnBehalf = if ($null -ne $mailbox.GrantSendOnBehalfTo ) { 
-                $sendOnBehalfCheck | ForEach-Object { 
-                    $sonbh = (Get-Recipient $_ -ErrorAction SilentlyContinue)
-                    if($null -ne $sonbh){$sonbh.PrimarySMTPAddress }
-                } 
-            }
-            [PSCustomObject]@{
-                DisplayName   = $mailbox.DisplayName
-                EmailAddress  = $mailbox.PrimarySMTPAddress
-                ReadManage    = $fullAccess.User -join ","
-                SendAs        = $sendAs.Trustee -join ","
-                SendOnBehalf  = $sendOnBehalf -join ","
+    function ProcessReport {
+        param (
+            [Parameter(Mandatory = $true)]
+            [object]$MailboxData,
+    
+            [Parameter(Mandatory = $true)]
+            [object]$PermissionData
+        )
+    
+        $userOrTrustee = $PermissionData.User
+        if ($null -eq $userOrTrustee) {
+            $userOrTrustee = $PermissionData.Trustee
+            if ($null -eq $userOrTrustee) {
+                $userOrTrustee = $MailboxData.PrimarySMTPAddress
             }
         }
-
+    
+        $permissions = $PermissionData.AccessRights
+        if ($null -eq $permissions -or $permissions.Count -eq 0) {
+            $permissions = "SendOnBehalf"
+        }
+    
+        [PSCustomObject]@{
+            DisplayName   = $MailboxData.DisplayName
+            EmailAddress  = $MailboxData.PrimarySMTPAddress
+            UserOrTrustee = $userOrTrustee
+            Permissions   = $permissions
+        }
     }
     
-    return $permReport
-#}
 
+    if ($MailboxAddress) {
+        # Fetch mailboxes in batch to reduce multiple Get-Mailbox calls
+        $mailboxes = @()
+        foreach ($user in $MailboxAddress) {
+            $mailboxes += Get-Mailbox -Identity $user -ErrorAction SilentlyContinue
+        }
+    }
+    else {
+        # Fetch mailboxes based on MailboxTypes
+        switch ($MailboxTypes) {
+            "UserMailbox" { $mailboxes = Get-Mailbox -RecipientTypeDetails UserMailbox -ResultSize Unlimited }
+            "SharedMailbox" { $mailboxes = Get-Mailbox -RecipientTypeDetails SharedMailbox -ResultSize Unlimited }
+            "RoomMailbox" { $mailboxes = Get-Mailbox -RecipientTypeDetails RoomMailbox -ResultSize Unlimited }
+            "All" { $mailboxes = Get-Mailbox -ResultSize Unlimited }
+        }
+    }
 
-$sendOnBehalf = if ($null -ne $sonbh ) { 
-    $sendOnBehalfCheck | ForEach-Object { 
-        $sonbh = (Get-Recipient $_ -ErrorAction SilentlyContinue)
-        if($null -ne $sonbh){$sonbh.PrimarySMTPAddress -join ","}
-    } 
+    foreach ($mailbox in $mailboxes) {
+        if ($null -ne $mailbox) {
+            # Fetch full access permissions
+            $fullAccess = Get-MailboxPermission -Identity $mailbox.Identity | Where-Object { $_.User -ne "NT AUTHORITY\SELF" }
+            # Fetch SendAs permissions
+            $sendAs = Get-RecipientPermission -Identity $mailbox.Identity | Where-Object { $_.Trustee -ne "NT AUTHORITY\SELF" }
+            # Fetch SendOnBehalf permissions
+            $sendOnBehalfCheck = $mailbox.GrantSendOnBehalfTo
+            $sendOnBehalf = if ($null -ne $sendOnBehalfCheck) {
+                $sendOnBehalfCheck | ForEach-Object { 
+                    $recipient = Get-Recipient $_ -ErrorAction SilentlyContinue
+                    if ($null -ne $recipient) { $recipient| Select-Object PrimarySMTPAddress }
+                }
+            }
+
+            if ($ExpandedReport) {
+                if ($null -ne $fullAccess) {
+                    $fullAccess | ForEach-Object {
+                        $permReport += ProcessReport -MailboxData $mailbox -PermissionData $_
+                    }
+                }
+                
+                if ($null -ne $sendAs) {
+                    $sendAs | ForEach-Object {
+                        $permReport += ProcessReport -MailboxData $mailbox -PermissionData $_
+                    }  
+                }
+
+                if ($null -ne $sendOnBehalfCheck) {
+                    $sendOnBehalf | ForEach-Object {
+                        $permReport += ProcessReport -MailboxData $mailbox -PermissionData $_
+                    } 
+                } 
+            }
+            else {
+                # Build the report object
+                $permReport += [PSCustomObject]@{
+                    DisplayName           = $mailbox.DisplayName
+                    EmailAddress          = $mailbox.PrimarySMTPAddress
+                    ReadManage            = ($fullAccess | Select-Object -ExpandProperty User) -join ";"
+                    ReadManagePermissions = ($fullAccess | Select-Object -ExpandProperty AccessRights) -join ";"
+                    SendAs                = ($sendAs | Select-Object -ExpandProperty Trustee) -join ";"
+                    SendOnBehalf          = ($sendOnBehalf) -join ";"
+                }
+            }
+        }
+    }
+
+    $permReport #| Export-Csv "./Reports/$(Get-Date -Format 'yyyyMMdd_HH_MM_ss').csv" -NoTypeInformation
 }
